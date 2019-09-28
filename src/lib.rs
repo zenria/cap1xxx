1223742ebe8a49ca75061bcb51cfc6f998544ebd
Philippe GASSMANN <philoops@gmail.com>
2019-09-28 09:35:37 +0200

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! cap1xxx [`embedded-hal`] I2C driver for Microchip cap1xxx capacitive touch buttons
+//!
+//! This crate is primarily targeting cap1666 6 buttons - 6 led hardware found
+//! on the Pimoroni GFX HAT
+
+use embedded_hal::blocking::i2c::{Read, Write};
+
 #[cfg(test)]
 mod tests {
     #[test]
